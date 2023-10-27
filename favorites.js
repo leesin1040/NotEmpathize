fdd26921bd6a022d7ff74b0ca6090ad89f2cf656
Your Name <you@example.com>
2023-10-27 15:56:07 +0900

    즐겨찾기 목록 클릭시 상세페이지 이동

--- favorites.js
+++ favorites.js
@@ -1,5 +1,4 @@
 import { movieDetailApi } from './detail.js';
-import { options } from './script.js';
 
 const favMainBtn = document.querySelector('#favoritesHome');
 const getfavArr = JSON.parse(localStorage.getItem('favorites'));
@@ -26,31 +25,6 @@ console.log('result =>', result);
 // 즐겨찾기 모달 목록에 삽입
 const modalBody = document.querySelector('#favContent');
 modalBody.innerHTML = result;
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko-kr&page=1', options)
-    .then((response) => response.json())
-    .then((response) => {
-        let results = response.results;
-
-        // for (let i = 0; i < getfavArr.length; i++) {
-        //     console.log(`getfavArr의 ${[i]}번재 id값 =`, getfavArr[i].id);
-        // }
-
-        // // api 리스트 중 id값 불러옴
-        // results.forEach((movie) => {
-        //     let movieInfoStr = {
-        //         titie: movie.title,
-        //         id: movie.id,
-        //     };
-        //     // console.log(movieInfoStr);
-        //     movieInfoArr.push(movieInfoStr);
-        // });
-        // console.log('api id값 불러오기 =>', movieInfoArr);
-
-        // // localsotrage id값과 api id값 비교 후 같은 거 뽑기
-
-        // let compareArr = getfavArr.some((data) => data.id == movieId);
-        // console.log('local,api id 교집합 =>', compareArr);
-    });
 
 // 즐겨찾기 버튼 클릭 이벤트
 favMainBtn.addEventListener('click', (e) => {
@@ -59,14 +33,21 @@ favMainBtn.addEventListener('click', (e) => {
 });
 
 // 즐겨찾기 목록 페이지 이동 이벤트
-const favListClk = document.querySelector('.favListArr');
-const favListClkId = document.querySelector('.favListArr').getAttribute('id');
-favListClk.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('목록 클릭!', favListClkId);
-    movieDetailApi(favListClkId);
+const favListClk = document.querySelectorAll('.favListArrBox');
+console.log('favListClk', favListClk);
+favListClk.forEach((clk) => {
+    clk.addEventListener('click', (e) => {
+        e.preventDefault();
+        // console.log(e.currentTarget.children[0].getAttribute('id'));
+        movieDetailApi(e.currentTarget.children[0].getAttribute('id'));
+    });
 });
 
+function listDeleteFunc(del) {
+    console.log(favListDelId);
+    console.log('콜백 함수 =>', del.id);
+    return del.id !== favListDelId;
+}
 // 즐겨찾기 목록 삭제 이벤트
 const favListDel = document.querySelector('.fa');
 const favListDelId = document.querySelector('.fa').getAttribute('id');
@@ -74,9 +55,3 @@ favListDel.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('삭제 클릭! =>', favListDelId);
 });
-
-// 페이지 이동 시 모달 창 닫기
-// function modaldown() {
-//     document.querySelector('#favorites-modalBox').style.display = 'none';
-//     document.querySelector('#favorites-modalBox').class = 'modal fade';
-// }
